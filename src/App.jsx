85c3cf5f98277f6500965a56339f643a56545ae1
fido1hn <confidoekeanya@gmail.com>
2025-04-30 23:32:52 +0100

    handleSubmit for FormAddFriend component

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleShowAddFriend() {
-    setShowAddFriend((val) => !val);
+    setShowAddFriend((show) => !show);
   }
 
   return (
@@ -87,13 +87,43 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+      id,
+    };
+
+    console.log(newFriend);
+
+    setImage("https://i.pravatar.cc/48");
+    setName("");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑 Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🌇 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add Friend</Button>
     </form>
